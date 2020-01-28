69704b25543f1e1fd87470662e48a940a0bbec6b
Marcellofabbri <marcellogfabbri@gmail.com>
2020-01-28 14:57:31 +0000

    authentication method

--- lib/user.rb
+++ lib/user.rb
@@ -3,9 +3,18 @@
 require_relative '../connection_setup'
 require 'bcrypt'
 
+
 class User < Sequel::Model
   def self.new_user(name:, email:, phone_number:, username:, password:)
     encrypted_password = BCrypt::Password.create(password)
     User.create(name: name, email: email, phonenumber: phone_number, username: username, password: encrypted_password.to_s)
+  end
+
+  def self.authenticate(username:, password:)
+    result = User.where(username: username)
+    return nil unless result.any?
+    return nil unless BCrypt::Password.new(result.first.password) == password
+    
+    result.first
   end
 end
