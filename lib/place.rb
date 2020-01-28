314a1f760e4f7f1ccbba961a7e9bda329ab538c4
Umberto Valente <uvalente89@gmail.com>
2020-01-28 11:38:29 +0000

    Created .new_place method for Place class

--- lib/place.rb
+++ lib/place.rb
@@ -3,4 +3,7 @@
 require_relative '../connection_setup'
 
 class Place < Sequel::Model
+  def self.new_place(user_id:, listing_title:, description:, address:, ppn:)
+    Place.create(userid: user_id, listingtitle: listing_title, description: description, address: address, ppn: ppn)
+  end
 end
