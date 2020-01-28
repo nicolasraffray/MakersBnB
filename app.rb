281f2bdb3c4a2ab382e8fc0a359d4ca77d1003e4
Marcellofabbri <marcellogfabbri@gmail.com>
2020-01-28 17:19:01 +0000

    add_listing functionality added

--- app.rb
+++ app.rb
@@ -1,30 +1,28 @@
 require 'sinatra/base'
 require './lib/place'
 require './lib/user'
+
 class Bnb < Sinatra::Base
   enable :sessions, :method_override
 
   get '/' do 
     @user = session[:user]
     erb :index
-
   end 
 
   post '/users' do 
    user = User.new_user(name: params[:name], email: params[:email], phone_number: params[:phonenumber], username: params[:username], password: params[:password])
    session[:user] = user
-  
    redirect ('/')
   end 
+
  get '/places' do 
     @places = Place.all
     erb :places
-
-
   end 
+
   get '/users/new' do 
     erb :'users/new'
-
   end 
   
   delete '/session' do 
@@ -39,6 +37,15 @@ class Bnb < Sinatra::Base
   post '/session' do
     session[:user] = User.authenticate(username: params[:username], password: params[:password])
     redirect ('/')
+  end
+
+  get '/places/new' do
+    erb :'places/new'
+  end
+
+  post '/places' do
+    Place.create(userid: session[:user].id, listingtitle: params[:listingtitle], description: params[:description], address: params[:address], ppn: params[:ppn])
+    redirect('/places')
   end
 
  run! if app_file == $0
--- spec/features/add_listing_spec.rb
+++ spec/features/add_listing_spec.rb
@@ -1,6 +1,18 @@
-xfeature "add listing" do
+feature "add listing" do
   scenario "it lets user add a new listing" do
     visit('/')
-    click_button('sign up')
+    user = User.new_user(name: 'Umberto', username: 'uValente', email: 'mock@gmail.com', password: 'psw123', phone_number: 123_456_789)
+    click_button('Log In')
+    fill_in 'username', with: 'uValente'
+    fill_in 'password', with: 'psw123'
+    click_button('Log In')
+    click_button('Add Listing')
+    fill_in 'listingtitle', with: 'Very cool penthouse in Milan'
+    fill_in 'description', with: 'Incredible apartment on the top floor of the highest skyscraper in Milan'
+    fill_in 'address', with: '76 Italy Road, Milan, 48012, IT'
+    fill_in 'ppn', with: '342'
+    click_button('Publish')
+    expect(page).to have_content('Very cool penthouse in Milan')
+    expect(page).to have_content('48012')
   end
 end
