01325b5cd2331dad17c6c2dd7e397df4d5d6acea
Umberto Valente <uvalente89@gmail.com>
2020-01-28 11:10:59 +0000

    .new_user create an entry in the database and return a instance of User

--- lib/user.rb
+++ lib/user.rb
@@ -6,5 +6,6 @@ require 'bcrypt'
 class User < Sequel::Model
   def self.new_user(name:, email:, phone_number:, username:, password:)
     encrypted_password = BCrypt::Password.create(password)
+    User.create(name: name, email: email, phonenumber: phone_number, username: username, password: encrypted_password.to_s)
   end
 end
